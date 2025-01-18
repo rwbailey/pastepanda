466f5813fd6a0b6b5278587c6c470e726bae5eae
Richard Bailey <richard@richardbailey.xyz>
2025-01-18 23:15:17 +0000

    add DI and logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,27 +2,38 @@ package main
 
 import (
 	"flag"
-	"log"
+	"log/slog"
 	"net/http"
+	"os"
 )
+
+type application struct {
+	logger *slog.Logger
+}
 
 func main() {
 	addr := flag.String("addr", ":4000", "The HTTP network address")
 	flag.Parse()
 
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		logger: logger,
+	}
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
 	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", home)
-	mux.HandleFunc("GET /paste/view/{id}", pasteView)
-	mux.HandleFunc("GET /paste/create", pasteCreate)
-	mux.HandleFunc("POST /paste/create", pasteCreatePost)
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /paste/view/{id}", app.pasteView)
+	mux.HandleFunc("GET /paste/create", app.pasteCreate)
+	mux.HandleFunc("POST /paste/create", app.pasteCreatePost)
 
-	log.Printf("starting server on %s", *addr)
+	logger.Info("starting server", slog.String("addr", *addr))
 
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err)
+	logger.Error(err.Error())
+	os.Exit(1)
 }
