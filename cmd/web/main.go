58b071d064e3c025387024ca9376b3ee06495595
Richard Bailey <richard@richardbailey.xyz>
2025-01-19 12:01:57 +0000

    move routes to own file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,19 +21,9 @@ func main() {
 		logger: logger,
 	}
 
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("GET /{$}", app.home)
-	mux.HandleFunc("GET /paste/view/{id}", app.pasteView)
-	mux.HandleFunc("GET /paste/create", app.pasteCreate)
-	mux.HandleFunc("POST /paste/create", app.pasteCreatePost)
-
 	logger.Info("starting server", slog.String("addr", *addr))
 
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	logger.Error(err.Error())
 	os.Exit(1)
 }
---
+++ cmd/web/routes.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func (app *application) routes() *http.ServeMux {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("GET /{$}", app.home)
+	mux.HandleFunc("GET /paste/view/{id}", app.pasteView)
+	mux.HandleFunc("GET /paste/create", app.pasteCreate)
+	mux.HandleFunc("POST /paste/create", app.pasteCreatePost)
+
+	return mux
+}
