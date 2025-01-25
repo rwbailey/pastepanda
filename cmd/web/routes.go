f85d6b3b50295eeb893d5150b13a90414597edd0
Richard Bailey <richard@richardbailey.xyz>
2025-01-25 12:22:42 +0000

    add alice composable middleware package

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
@@ -13,9 +17,11 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("GET /paste/create", app.pasteCreate)
 	mux.HandleFunc("POST /paste/create", app.pasteCreatePost)
 
-	return app.recoverPanic(
-		app.logRequest(
-			commonHeaders(mux),
-		),
+	standard := alice.New(
+		app.recoverPanic,
+		app.logRequest,
+		commonHeaders,
 	)
+
+	return standard.Then(mux)
 }
