b62b202621f3b448ac01d455a21bdb6374556224
Richard Bailey <richard@richardbailey.xyz>
2025-02-01 20:29:49 +0000

    add user model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,6 +21,7 @@ import (
 type application struct {
 	logger         *slog.Logger
 	pastes         *models.PasteModel
+	users          *models.UserModel
 	templateCache  map[string]*template.Template
 	formDecoder    *form.Decoder
 	sessionManager *scs.SessionManager
@@ -56,6 +57,7 @@ func main() {
 	app := &application{
 		logger:         logger,
 		pastes:         &models.PasteModel{DB: db},
+		users:          &models.UserModel{DB: db},
 		templateCache:  templateCache,
 		formDecoder:    formDecoder,
 		sessionManager: sessionManager,
