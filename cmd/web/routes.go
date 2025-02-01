4284486821fb5deee7be891a1de4e6ee7ce838cd
Richard Bailey <richard@richardbailey.xyz>
2025-02-01 20:17:41 +0000

    add auth routes

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -19,6 +19,12 @@ func (app *application) routes() http.Handler {
 	mux.Handle("GET /paste/create", dynamic.ThenFunc(app.pasteCreate))
 	mux.Handle("POST /paste/create", dynamic.ThenFunc(app.pasteCreatePost))
 
+	mux.Handle("GET /user/signup", dynamic.ThenFunc(app.userSignup))
+	mux.Handle("POST /user/signup", dynamic.ThenFunc(app.userSignupPost))
+	mux.Handle("GET /user/login", dynamic.ThenFunc(app.userLogin))
+	mux.Handle("POST /user/login", dynamic.ThenFunc(app.userLoginPost))
+	mux.Handle("POST /user/logout", dynamic.ThenFunc(app.userLogoutPost))
+
 	standard := alice.New(
 		app.recoverPanic,
 		app.logRequest,
