fa4085b72d3d2e952674d42db96b849e37b62f14
Richard Bailey <richard@richardbailey.xyz>
2025-02-02 17:59:49 +0000

    embed static files

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,13 +4,14 @@ import (
 	"net/http"
 
 	"github.com/justinas/alice"
+
+	"pastepanda/ui"
 )
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("GET /static/", http.FileServerFS(ui.Files))
 
 	dynamic := alice.New(
 		app.sessionManager.LoadAndSave,
