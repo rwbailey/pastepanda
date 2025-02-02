cac0c7c21df01767ca6d3b78c4345c7c479d17b9
Richard Bailey <richard@richardbailey.xyz>
2025-02-02 17:41:29 +0000

    change logged in check to use context and check database

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -15,6 +15,7 @@ func (app *application) routes() http.Handler {
 	dynamic := alice.New(
 		app.sessionManager.LoadAndSave,
 		noSurf,
+		app.authenticate,
 	)
 
 	mux.Handle("GET /{$}", dynamic.ThenFunc(app.home))
